3588ca4f04e9d073707cd60b8f3552e11ce63785
JirayuSrisawat-Github <contact@jirayu.pw>
2023-12-13 07:38:29 +0700

    OpenGraph

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -3,8 +3,13 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Jirayu Srisawat | จิรายุ ศรีสวัสดิ์",
   description: "เว็บไซต์แนะนำตัวเองของจิรายุ ศรีสวัสดิ์",
-  icons: [{
-    url: '/profile.png',
-    href: '/profile.png'
-  }]
+  icons: [
+    {
+      url: "/profile.png",
+      href: "/profile.png"
+    }
+  ],
+  openGraph: {
+    images: ["https://storage.jirayu.pw/00016-1315674954.png"]
+  }
 };
